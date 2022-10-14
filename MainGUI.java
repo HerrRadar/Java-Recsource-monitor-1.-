ba1b2172aece2e72462ba2b2d7457edebcc91d93
Ecte86 <djfran86@gmail.com>
2022-10-14 18:27:18 +1030

    I admit defeat. No idea how this works properly.

--- MainGUI.java
+++ MainGUI.java
@@ -2,44 +2,43 @@ import org.knowm.xchart.*;
 
 public class MainGUI {
 
-  public interface CPUinterface { //interface
+  public interface CPUinterface { // interface
     public void cpuloadfile();
+
     public void cpufile();
+
     public void cpucal();
+
     public double CPULOADcal();
   }
 
-  class Main //Main thread
+  class Main // Main thread
   {
-    public static < CPU > void main(String[] args) throws InterruptedException {
+    public static <CPU> void main(String[] args) throws InterruptedException {
       // variables for the chart
       cpucal myCPU = new cpucal();
-      double x1 = 0.0;
-      double x2 = 0.0;
-      double x3 = 0.0;
-      do {
-        x1 = myCPU.CPULOADcal();
-        Thread.sleep(1000);
-        x2 = myCPU.CPULOADcal();
-        Thread.sleep(1000);
-        x3 = myCPU.CPULOADcal();
-        break;
-      } while (true);
-      double[] xData = new double[] {
-        0.0,
-        30.0,
-        60.0
-      };
-      double[] yData = new double[] {
-        x1,
-        x2,
-        x3
-      };
-      System.out.println("Running program:  " + "x1 value:  " + x1 + "x2 value:  " + x2 + "x3 value:  " + x3);
-      // creating the Chart
-      XYChart chart = QuickChart.getChart("chart", "X", "Y", "y(x)", xData, yData);
+      int numValues = 10;
+      double[] yValues = new double[numValues];
+      double[] xLabels = new double[numValues];
+      double[] yData = new double[numValues];
+      for (int i = 0; i < numValues; i++) {
+        double load = myCPU.CPULOADcal();
+        if (load > 100) {
+          load = load % 100;
+        }
+        yValues[i] = load;
+        xLabels[i] = 30 * i; // starts at 0, goes up in multiples of 30
+        yData[i] = yValues[i];
+        // Thread.sleep(1000);
+      }
+      System.out.println("Running program: ");
+      for (int j=0; j < numValues; j++) {
+        System.out.println("x" + (j + 1) + " value: " + yValues[j] + "\t");
+      } // + "x1 value: " + x1 + "x2 value: " + x2 + "x3 value: " + x3);
+        // creating the Chart
+      XYChart chart = QuickChart.getChart("chart", "X (Time)", "Y (CPU Load)", "x(y) CPU Load per sec", xLabels, yData);
 
-      new SwingWrapper < > (chart).displayChart();
+      new SwingWrapper<>(chart).displayChart();
     }
   }
 }
--- cpuloadfile.java
+++ cpuloadfile.java
@@ -1,22 +1,91 @@
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class cpuloadfile {
-  public static String CPUfile() throws InterruptedIOException {
+  boolean logFileDirCreated = false;
+  boolean logFileCreated = false;
+
+  public String CPUfile() throws InterruptedIOException {
     String conf = "";
-    try {
-      File log = new File("logs\\CPULOG.txt");
-      if (log.createNewFile()) {
-        System.out.println("File created: " + log.getName());
-        conf = "File created:  " + log.getName();
-      } else {
-        System.out.println("File already exists.");
-        conf = "File already exsists";
+    if (this.logFileCreated == false) {
+      try {
+        Path logFilePath = Path.of("logs/CPULOG.txt");
+        if (logFilePath.toFile().exists()) {
+          return "";
+        }
+        Files.createDirectory(logFilePath);
+        File log = new File("logs/CPULOG.txt");
+        if (createLogFile(logFilePath.toString())) {
+          System.out.println("File created: " + log.getName());
+          conf = "File created:  " + log.getName();
+          this.logFileCreated = true;
+          this.logFileDirCreated = true;
+        } else {
+          System.out.println("File already exists.");
+          conf = "File already exists";
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+        System.exit(1);
       }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
     }
     return conf;
+  }
 
+  private Boolean createLogFile(String logFilePathString) throws IOException {
+
+    String directoryPathString = logFilePathString.split("/")[0];
+    Path logDirectoryPath = Path.of(directoryPathString);
+    String fileNameString = logFilePathString.split("/")[1];
+    Path logFilePath = Path.of(directoryPathString, fileNameString);
+    try {
+      if (createDirectory(logDirectoryPath) && createFile(logFilePath)) {
+        this.logFileCreated = true;
+        this.logFileDirCreated = true;
+        return true;
+      } else {
+        return false;
+      }
+    } catch (IOException e) {
+      if (e.getCause() != null && e.getCause() instanceof FileAlreadyExistsException)
+        return true;
+
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+      System.exit(1);
+      return false;
+    }
+  }
+
+  private Boolean createDirectory(Path dirPath) throws InterruptedIOException {
+    try {
+      Files.createDirectory(dirPath);
+      return true;
+    } catch (FileAlreadyExistsException e) {
+      return true;
+    } catch (IOException e) {
+      if (e.getCause() != null && e.getCause() instanceof FileAlreadyExistsException)
+        return true;
+      System.out.println("An error occurred creating dir.");
+      e.printStackTrace();
+      System.exit(1);
+      return false;
+    }
+  }
+
+  private Boolean createFile(Path filePath) throws InterruptedIOException {
+    try {
+      Files.createFile(filePath);
+      return true;
+    } catch (FileAlreadyExistsException e) {
+      return true;
+    } catch (IOException e) {
+      System.out.println("An error occurred creating file.");
+      e.printStackTrace();
+      return false;
+    }
   }
 }
